c71e8ad1df888863c98eab9e06cc89f0d37a1234
Anandhan <anandhanedom@gmail.com>
2020-07-30 16:57:04 +0530

    till chapter 3

---
+++ 012_js_programming/00003_head_first_js/03_chapter/01_pass_by_value/app.js
@@ -0,0 +1,16 @@
+var age = 7;
+
+function addOne(x) {
+  x = x + 1;
+  // console.log(x);
+}
+
+addOne(age);
+
+function doIt(x) {
+  x = 2;
+}
+
+doIt(age);
+
+console.log(age);
---
+++ 012_js_programming/00003_head_first_js/03_chapter/02_weird_functions/app.js
@@ -0,0 +1,10 @@
+function makeTea(cups, tea) {
+  console.log('Brewing ' + cups + ' cups of ' + tea);
+}
+// makeTea(3);
+makeTea(3, 'Earl Grey', 'hey ma!', 42);
+
+function barkAtTheMoon() {
+  console.log('Woooooooooooooo!');
+}
+barkAtTheMoon();
