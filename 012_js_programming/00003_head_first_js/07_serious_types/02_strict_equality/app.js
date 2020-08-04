2142547badd08bd771f15e231fd5958e1aea696f
Anandhan <anandhanedom@gmail.com>
2020-08-04 11:48:48 +0530

    chapter 7 progressing

---
+++ 012_js_programming/00003_head_first_js/07_serious_types/02_strict_equality/app.js
@@ -0,0 +1,42 @@
+// var test11 = 0 / 0;
+// console.log(typeof test11);
+
+// console.log('42' == 42);
+// console.log('0' == 0);
+// console.log('0' == false);
+// console.log('true' == true);
+// console.log(true == (1 == '1'));
+
+// console.log('42' === 42);
+// console.log('0' === 0);
+// console.log('0' === false);
+// console.log('true' === true);
+// console.log(true === (1 === '1'));
+
+// var mul = 3 * '4';
+// var div = 80 / '10';
+// var add = 3 + '4';
+// var plus = '4' + 3;
+
+// console.log(mul, div, add, plus);
+
+let ans = Infinity - 1;
+// console.log(ans);
+
+ans = 1 + 2 + ' pizzas';
+// ans=(1 + 2) + " pizzas";
+ans = 1 + (2 + ' pizzas');
+
+ans = -true;
+ans = 3 + Number('4');
+
+ans = Infinity - '1';
+ans = '42' + 42;
+ans = 2 + '1 1';
+ans = 99 + 101;
+
+ans = '1' - '1';
+ans = 10 / 2;
+ans = 3 + ' bananas ' + 2 + ' apples';
+
+console.log(ans);
