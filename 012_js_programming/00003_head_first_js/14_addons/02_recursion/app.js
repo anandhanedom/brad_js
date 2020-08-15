da744c6a3db7d468ea95375942ecf8c89b7812e9
Anandhan <anandhanedom@gmail.com>
2020-08-15 17:03:21 +0530

    completed

--- 012_js_programming/00003_head_first_js/14_addons/02_recursion/app.js
+++ 012_js_programming/00003_head_first_js/14_addons/02_recursion/app.js
@@ -1,4 +1,11 @@
-function fibonacci() {
-  if (n === 0) {
+function fibonacci(num) {
+  if (num === 1 || num === 0) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log(`Fibonacci of ${i} is ${fibonacci(i)}`);
 }
